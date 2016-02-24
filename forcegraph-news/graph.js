9cf48b7d8c2d1e582bb174d35188819034cb4590
Matthew Henkler <matt@henkler.org>
2016-02-23 20:01:18 -0600

    Finished up forcegraph code

--- forcegraph-news/graph.js
+++ forcegraph-news/graph.js
@@ -1,85 +1,85 @@
-function renderForceMap(dataPath) {
-  var outerWidth = 800;
-  var outerHeight = 800;
-  var margin = {
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0
-  };
+var forcemap = function()  {
+  var outerWidth;
+  var outerHeight;
+  var margin;
+  var innerWidth;
+  var innerHeight;
+  var minNodeRadius = 16;
+  var baseNodeRadius = 2;
+  var force;
+  var svg;
+  var g;
 
-  var innerWidth = outerWidth - margin.left - margin.right;
-  var innerHeight = outerHeight - margin.top - margin.bottom;
+  var dataPath;
+  var data;
 
-  var svg = d3.select("#forcemap").append("svg")
-    .attr("width", outerWidth)
-    .attr("height", outerHeight);
-  var g = svg.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  var timer = null;
 
-  function render(data, transform, mouseOver, mouseOut) {
-    var transformedData = transform(data);
-    var nodes = transformedData.nodes;
-    var links = transformedData.links;
+  var init = function(path, width, height) {
+    dataPath = path;
+    outerWidth = width;
+    outerHeight = height;
+    margin = {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    };
 
-    var force = d3.layout.force().charge(-1000)
+    innerWidth = outerWidth - margin.left - margin.right;
+    innerHeight = outerHeight - margin.top - margin.bottom;
+
+    force = d3.layout.force()
       .size([innerWidth, innerHeight])
-      .nodes(nodes)
-      .links(links)
+      .gravity(0.15)
+      .linkDistance(function(link) { return 20 + getNodeRadius(link.source) + getNodeRadius(link.target); })
+      .charge(function(node) { return -100 * node.weight; })
       .on("tick", forceTick);
 
-    var linkLines = g.selectAll("line.link")
-      .data(links, function(d) { return d.key; });
-    linkLines.enter()
-      .append("line")
-      .attr("class", "link")
-      .style("stoke", "black")
-      .style("opacity", 0.5)
-    linkLines
-      .style("stroke-width", function(d) { return d.weight; });
-    linkLines.exit().remove();
+    svg = d3.select("#forcemap").append("svg")
+      .attr("width", innerWidth)
+      .attr("height", innerHeight);
 
-    var nodeGroups = g.selectAll("g.node")
-      .data(nodes, function(d) { return d.key; });
-    nodeGroups.enter()
-      .append("g")
-      .attr("class", "node");
-    nodeGroups.exit().remove();
-
-    var nodeCircles = nodeGroups.selectAll("circle").data(function(d) { return [d]; });
-    nodeCircles.enter()
+    svg.append("clipPath")
+      .attr("id", "circleClip")
       .append("circle");
-    nodeCircles
-      .attr("r", 5)
-      .attr("fill", "lightgray");
-    nodeCircles.exit().remove();
 
-    force.start();
-
-    d3.select("body").append("pre")
-  .text(JSON.stringify(nodes, null, 2));
-  d3.select("body").append("pre")
-.text(JSON.stringify(links, null, 2));
+    g = svg.append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
   }
 
-  function forceTick() {
-    d3.selectAll("line.link")
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
-    d3.selectAll("g.node")
-      .attr("transform", function(d) { return "translate("+ d.x +"," + d.y + ")"; });
+  var start = function() {
+    if (force && !timer) {
+      // reload the node list every minute
+      timer = setInterval(function() { renderMapData(); }, 60 * 1000);
+      renderMapData();
+    }
   }
 
-  function transformToNodesAndLinks(data) {
+  var stop = function() {
+    if (timer) {
+      clearInterval(timer);
+      timer = null;
+    }
+  }
+
+  var renderMapData = function() {
+    d3.json(dataPath, function(error, rawdata) {
+      if (error) throw error;
+
+      data = transformToNodesAndLinks(rawdata);
+      render();
+    });
+  }
+
+  var transformToNodesAndLinks = function(rawdata) {
     var nodes = [];
     var nodeAuthorMap = {};
     var nodeStoryMap = {};
     var links = [];
     var linkMap = {};
 
-    data.forEach(function(story) {
+    rawdata.forEach(function(story) {
       // create the author as a node
       var authorID = story.author.username;
       var authorNode = nodeAuthorMap[authorID];
@@ -88,15 +88,10 @@ function renderForceMap(dataPath) {
         authorNode = {
           key: authorID,
           type: 'author',
-          picture: story.author.picture,
-          storyCount: 1
+          imageURL: story.author.picture
         }
         nodes.push(authorNode);
         nodeAuthorMap[authorNode.key] = authorNode;
-      }
-      else {
-        // increment the storycount if we've seen this author before
-        authorNode.storyCount++;
       }
 
       // create the story as a node
@@ -105,14 +100,10 @@ function renderForceMap(dataPath) {
       if (!storyNode) {
         storyNode = {
           key: storyID,
-          type: 'story',
-          storyCount: 1
+          type: 'story'
         }
         nodes.push(storyNode);
         nodeStoryMap[storyNode.key] = storyNode;
-      }
-      else {
-        storyNode.storyCount++;
       }
 
       // create an edge between author and story.  If we already have an edge, increment the weight
@@ -125,12 +116,12 @@ function renderForceMap(dataPath) {
           target: storyNode,
           weight: 1
         }
-        links.push(link);
-        linkMap[link.key] = link;
       }
       else {
         link.weight++;
       }
+      links.push(link);
+      linkMap[link.key] = link;
     });
 
     return {
@@ -139,37 +130,107 @@ function renderForceMap(dataPath) {
     };
   }
 
-  function getHostNameFromURL(url) {
+  var getHostNameFromURL = function(url) {
     var a = document.createElement('a');
     a.href = url;
     return a.hostname.replace(/^www\./, '');
   }
 
-  function nodeMouseOver(node) {
-    /*
+  var render = function() {
+    var nodes = data.nodes;
+    var links = data.links;
+
+    force.nodes(nodes)
+      .links(links)
+      .start();
+
+    var linkLines = g.selectAll("line.link")
+      .data(links, function(d) { return d.key; });
+    linkLines.enter()
+      .append("line")
+      .attr("class", "link")
+      .style("stroke", "black")
+      .style("opacity", 0.5);
+    linkLines
+      .attr("stroke-width", function(d) { return 2 * d.weight; });
+    linkLines.exit()
+      .transition()
+      .duration(3000)
+      .style("opacity", 0)
+      .remove();
+
+    var nodeGroups = g.selectAll("g.node")
+      .data(nodes, function(d) { return d.key; });
+    nodeGroups.enter()
+      .append("g")
+      .attr("class", "node")
+      .call(force.drag())
+      .on("mouseover", nodeMouseOver)
+      .on("mouseout", nodeMouseOut);
+    nodeGroups.exit()
+      .transition()
+      .duration(3000)
+      .style("opacity", 0)
+      .remove();
+
+    var nodeCircles = nodeGroups.selectAll("circle").data(function(d) { return [d]; });
+    nodeCircles.enter()
+      .append("circle");
+    nodeCircles
+      .attr("r", function(d) { return getNodeRadius(d); })
+      .attr("fill", function(d) { return d.type === 'author' ? 'green' : 'blue'; })
+      .attr("stroke", "black")
+      .attr("stroke-width", 2);
+    nodeCircles.exit().remove();
+
+    var authorNodeImages = nodeGroups.filter(function(d) { return d.type === 'author'; })
+      .selectAll("image.node-image").data(function(d) { return [d]; });
+    authorNodeImages.enter()
+      .append("image")
+      .attr("class", "node-image");
+    authorNodeImages
+      .attr("xlink:href", function(d) { return d.imageURL; })
+      .attr("clip-path", "url(#circleClip)");
+    authorNodeImages.exit().remove();
+  }
+
+  var getNodeRadius = function(node) {
+    return minNodeRadius + baseNodeRadius * (node.weight - 1);
+  }
+
+  var forceTick = function() {
+    d3.selectAll("line.link")
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+    d3.selectAll("g.node")
+      .attr("transform", function(d) { return "translate("+ d.x +"," + d.y + ")"; });
+  }
+
+  var nodeMouseOver = function(node) {
     var toolTip = d3.select("#tooltip");
-    var html = "<h4>" + month.year + " - " + month.monthName + "</h4>";
-    html += "<h5>Temperature: " + month.temperature.toFixed(2) + "&deg C<br>";
-    html += "(" + month.variance.toFixed(2) + "&deg C)</h5>";
+    var html = "<h4>" + node.key + " (" + node.weight + ")</h4>";
 
     toolTip.html(html)
       .style("left", (d3.event.pageX) + "px")
       .style("top", (d3.event.pageY) + "px")
-      .style("opacity", 0.9);*/
+      .style("opacity", 0.9);
   }
 
-  function nodeMouseOut() {
+  var nodeMouseOut = function() {
     d3.select("#tooltip").style("opacity", 0);
   }
 
-  d3.json(dataPath, function(error, data) {
-    if (error) throw error;
-
-    render(data, transformToNodesAndLinks, nodeMouseOver, nodeMouseOut);
-  });
-}
+  return {
+    init: init,
+    start: start,
+    stop: stop
+  };
+} ();
 
 $(document).ready(function() {
   var dataPath = "http://www.freecodecamp.com/news/hot";
-  renderForceMap(dataPath);
+  forcemap.init(dataPath, 900, 900);
+  forcemap.start();
 });
